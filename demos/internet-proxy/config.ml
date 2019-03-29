3f58cac4cbe38c11aa3ee9446f57964c68227faa
Darren Ldl <darrenldldev@gmail.com>
2019-03-29 19:24:58 +1100

    Moving back to single interface design for internet-proxy

--- demos/internet-proxy/config.ml
+++ demos/internet-proxy/config.ml
@@ -9,32 +9,23 @@ let packages = [
 let main =
   foreign
     ~packages
-    "Unikernel.Main" (console @-> network @-> network @-> ethernet @-> ethernet @-> arpv4 @-> arpv4 @-> ipv4 @-> ipv4 @-> job)
+    "Unikernel.Main" (console @-> network @-> ethernet @-> arpv4 @-> ipv4 @-> job)
 
-let net_A = netif ~group:"side-A" "fjjidosjofnoisjos"
-let ethif_A = etif net_A
+let net = default_network
+let ethif = etif net
 
-let net_B = netif ~group:"side-B" "1fjdiso"
-let ethif_B = etif net_B
+let arp = arp ethif
 
-let arp_A = arp ethif_A
-let arp_B = arp ethif_B
-
-let ipv4_A =
+let ipv4 =
   let addr = Ipaddr.V4.make 10 0 0 10 in
   let config = {
     network = (Ipaddr.V4.Prefix.make 24 addr, addr);
     gateway = None;
   } in
-  create_ipv4 ~config ethif_A arp_A
+  create_ipv4 ~config ethif arp
 
-let ipv4_B =
-  let addr = Ipaddr.V4.make 11 0 0 10 in
-  let config = {
-    network = (Ipaddr.V4.Prefix.make 24 addr, addr);
-    gateway = None;
-  } in
-  create_ipv4 ~config ethif_B arp_B
+let ipv4 =
+  create_ipv4 ethif arp
 
 let () =
-  register "ft-demo0" [ main $ default_console $ net_A $ net_B $ ethif_A $ ethif_B $ arp_A $ arp_B $ ipv4_A $ ipv4_B ]
+  register "ft-demo0" [ main $ default_console $ net $ ethif $ arp $ ipv4 ]
--- demos/internet-proxy/unikernel.ml
+++ demos/internet-proxy/unikernel.ml
@@ -2,32 +2,18 @@ open Lwt.Infix
 open Mirage_types_lwt
 open Firewall_tree
 
-module Main (C : CONSOLE) (N_A : NETWORK) (N_B : NETWORK) (E_A : ETHERNET) (E_B : ETHERNET) (A_A : ARP) (A_B : ARP) (I4_A : IPV4) (I4_B : IPV4) = struct
-  let start c n_A n_B e_A e_B a_A a_B i4_A i4_B =
-    Lwt.pick
-      [ N_A.listen n_A ~header_size:Ethernet_wire.sizeof_ethernet
-          (E_A.input
-             ~arpv4:(A_A.input a_A)
-             ~ipv4:(I4_A.input
-                      ~tcp:(fun ~src:src_addr ~dst:dst_addr data ->
-                          Lwt.return_unit
-                        )
-                      ~udp:(fun ~src:src_addr ~dst:dst_addr data -> Lwt.return_unit)
-                      ~default:(fun ~proto ~src:_ ~dst:_ _data -> C.log c "ping on side A")
-                      i4_A)
-             ~ipv6:(fun _ -> Lwt.return_unit)
-             e_A) >>= (fun _ -> Lwt.return_unit)
-      ; N_B.listen n_B ~header_size:Ethernet_wire.sizeof_ethernet
-          (E_B.input
-             ~arpv4:(A_B.input a_B)
-             ~ipv4:(I4_B.input
-                      ~tcp:(fun ~src:src_addr ~dst:dst_addr data ->
-                          Lwt.return_unit
-                        )
-                      ~udp:(fun ~src:src_addr ~dst:dst_addr data -> Lwt.return_unit)
-                      ~default:(fun ~proto ~src:_ ~dst:_ _data -> C.log c "ping on side B")
-                      i4_B)
-             ~ipv6:(fun _ -> Lwt.return_unit)
-             e_B) >>= (fun _ -> Lwt.return_unit)
-      ]
+module Main (C : CONSOLE) (N : NETWORK) (E : ETHERNET) (A : ARP) (I4 : IPV4) = struct
+  let start c n e a i4 =
+    N.listen n ~header_size:Ethernet_wire.sizeof_ethernet
+      (E.input
+         ~arpv4:(A.input a)
+         ~ipv4:(I4.input
+                  ~tcp:(fun ~src:src_addr ~dst:dst_addr data ->
+                      Lwt.return_unit
+                    )
+                  ~udp:(fun ~src:src_addr ~dst:dst_addr data -> Lwt.return_unit)
+                  ~default:(fun ~proto ~src:_ ~dst:_ _data -> C.log c "ping on side A")
+                  i4)
+         ~ipv6:(fun _ -> Lwt.return_unit)
+         e) >>= (fun _ -> Lwt.return_unit)
 end
