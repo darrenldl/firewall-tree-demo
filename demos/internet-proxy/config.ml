5395f681c5c3b44616c6f6f299f81f4fe6c56fd6
Darren Ldl <darrenldldev@gmail.com>
2019-03-29 14:57:49 +1100

    Progress on internet proxy

--- demos/internet-proxy/config.ml
+++ demos/internet-proxy/config.ml
@@ -3,12 +3,32 @@ open Mirage
 let packages = [
   package "ethernet";
   package "firewall-tree";
+  package "tcpip";
 ]
 
 let main =
   foreign
     ~packages
-    "Unikernel.Main" (console @-> network @-> job)
+    "Unikernel.Main" (console @-> network @-> ethernet @-> ipv4 @-> ipv6 @-> job)
+
+let net = default_network
+let ethif = etif net
+
+let ipv4 =
+  let addr = Ipaddr.V4.make 10 0 0 10 in
+  let config = {
+    network = (Ipaddr.V4.Prefix.make 24 addr, addr);
+    gateway = None;
+  } in
+  create_ipv4 ethif (arp ethif)
+
+let ipv6 =
+  let config = {
+    addresses = [];
+    netmasks  = [];
+    gateways  = [];
+  } in
+  create_ipv6 ethif config
 
 let () =
-  register "ft-demo0" [ main $ default_console $ default_network ]
+  register "ft-demo0" [ main $ default_console $ default_network $ ethif $ ipv4 $ ipv6 ]
--- demos/internet-proxy/unikernel.ml
+++ demos/internet-proxy/unikernel.ml
@@ -2,8 +2,20 @@ open Lwt.Infix
 open Mirage_types_lwt
 open Firewall_tree
 
-module Main (C : CONSOLE) (N : NETWORK) = struct
-  let start c n =
+module Main (C : CONSOLE) (N : NETWORK) (E : ETHERNET) (I4 : IPV4) (I6 : IPv6) = struct
+  let start c n e i4 i6 =
     N.listen n ~header_size:Ethernet_wire.sizeof_ethernet
-      (fun _ -> C.log c "Testing")
+      (E.input
+         ~arpv4:(fun _ -> Lwt.return_unit)
+         ~ipv4:(I4.input
+                  ~tcp:(fun ~src:src_addr ~dst:dst_addr data ->
+                      
+                      Lwt.return unit
+                    )
+                  ~udp:(fun _ -> Lwt.return unit)
+                  ~default:(fun ~proto _ -> Lwt.return unit)
+               i4)
+         ~ipv6:(fun _ -> Lwt.return_unit)
+         e)
+    >>= (fun _ -> Lwt.return_unit)
 end
